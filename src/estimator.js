a55f8d9b09f0b49bc51cd0b03b2bd6d464f8c7e0
IvyMachogu <ivy.machogu@strathmore.edu>
2020-04-11 13:19:14 +0300

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,27 @@
-const covid19ImpactEstimator = (data) => data;
+
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {};
+
+  /* getting  time to days */
+  if (data.periodType === 'weeks') {
+    data.timeToElapse = 7;
+  } else if (data.periodType === 'months') data.timeToElapse = 58;
+
+  const days = data.timeToElapse;
+  const factor = Math.trunc(days / 3);
+  const reportedCases = 2747;
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+
+  return {
+    impact,
+    severeImpact,
+    data
+
+  };
+};
 
 export default covid19ImpactEstimator;
