faaa8739af2332ee97441af133d556c30520d659
IvyMachogu <ivy.machogu@strathmore.edu>
2020-04-12 22:07:30 +0300

    challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -15,20 +15,28 @@ const factor = (data) => {
 const normalCases = (data) => (data.reportedCases * 10) * (2 ** factor(data));
 const severeCases = (data) => (data.reportedCases * 50) * (2 ** factor(data));
 const beds = (data) => Math.trunc(0.35 * data.totalHospitalBeds);
+const income = (data) => data.region.avgDailyIncomeInUSD;
+const population = (data) => data.region.avgDailyIncomePopulation;
+
 const covid19ImpactEstimator = (data) => ({
   data,
   impact: {
     currentlyInfected: data.reportedCases * 10,
     infectionsByRequestedTime: (normalCases(data)),
     severeCasesByRequestedTime: 0.15 * (normalCases(data)),
-    hospitalBedsByRequestedTime: Math.trunc((beds(data)) - Math.trunc(0.15 * (normalCases(data))))
-
+    hospitalBedsByRequestedTime: Math.trunc((beds(data)) - Math.trunc(0.15 * (normalCases(data)))),
+    casesForICUByRequestedTime: 0.05 * (normalCases(data)),
+    casesForVentilationByRequestedTime: 0.02 * (normalCases(data)),
+    dollarsInflight: (normalCases(data)) * income * population * (2 ** factor(data))
   },
   severeImpact: {
     currentlyInfected: data.reportedCases * 50,
     infectionsByRequestedTime: severeCases(data),
     severeCasesByRequestedTime: 0.15 * (severeCases(data)),
-    hospitalBedsByRequestedTime: Math.trunc((beds(data)) - Math.trunc(0.15 * (severeCases(data))))
+    hospitalBedsByRequestedTime: Math.trunc((beds(data)) - Math.trunc(0.15 * (severeCases(data)))),
+    casesForICUByRequestedTime: 0.05 * severeCases(data),
+    casesForVentilationByRequestedTime: 0.02 * severeCases(data),
+    dollarsInflight: severeCases(data) * income * population * (2 ** factor(data))
   }
 });
 
